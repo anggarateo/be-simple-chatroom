b9b3c8bfe1a8dd108b32a153c57ba9b1e5506a21
anggarateo <114375533+anggarateo@users.noreply.github.com>
2024-11-22 22:41:14 +0700

    Update cors.rb: add allowed origins (fe production)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins [
+      "http://localhost:3000",
+      "https://fe-simple-chatroom.vercel.app/
+    ]
 
     resource "*",
       headers: :any,
